c2edf752db02089fdb2eb7f4a0e1acf255ba9ae2
Irfana995 <58158728+Irfana995@users.noreply.github.com>
2019-12-07 14:05:42 +0530

    Add files via upload

---
+++ inheritclass.java
@@ -0,0 +1,23 @@
+package case5dec;
+
+public class inheritclass implements interface1,interface2{
+
+	public void add(int a,int b)
+	{
+		System.out.println(a+"+"+b+"="+(a+b));
+	}
+	public void sub(int a,int b)
+	{
+		System.out.println(a+"-"+b+"="+(a-b));
+	}
+	public void mul(int a,int b)
+	{
+		System.out.println(a+"*"+b+"="+(a*b));
+	}
+	public void div(int a,int b)
+	{
+		System.out.println(a+"/"+b+"="+(a/b));
+	}
+	
+	
+}
